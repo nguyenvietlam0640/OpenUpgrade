9eb1fa56e6715275d4ee23b3ed89603d3a56ca2b
nguyenvietlam0640 <lamnv0640@gmail.com>
2023-07-06 16:57:28 +0700

    [MIG] website_slides: migrate 15 to 16

---
+++ openupgrade_scripts/scripts/website_slides/16.0.2.6/post-migration.py
@@ -0,0 +1,11 @@
+from openupgradelib import openupgrade
+
+
+def recompute_slide_type(env):
+    env["slide.slide"].search([])._compute_slide_type()
+
+
+@openupgrade.migrate()
+def migrate(env, version):
+    openupgrade.load_data(env.cr, "website_slides", "16.0.2.6/noupdate_changes.xml")
+    recompute_slide_type(env)
