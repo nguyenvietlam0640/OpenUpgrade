7aa4d0c15a64d512380bbdf79456befbc6fd6a54
Roy Le <roy.le@viindoo.com>
2023-07-25 15:18:44 +0700

    fix

--- openupgrade_scripts/scripts/website_slides/16.0.2.6/post-migration.py
+++ openupgrade_scripts/scripts/website_slides/16.0.2.6/post-migration.py
@@ -1,20 +1,6 @@
 from openupgradelib import openupgrade
 
 
-def _fill_values_on_slide_slide(env):
-    slides = env["slide.slide"].with_context(active_test=False).search([])
-    slides._compute_slide_type()
-    slides._compute_slides_statistics()
-
-
-def _fill_nbr_article_on_slide_channel(env):
-    env["slide.channel"].with_context(active_test=False).search(
-        []
-    )._compute_slides_statistics()
-
-
 @openupgrade.migrate()
 def migrate(env, version):
     openupgrade.load_data(env.cr, "website_slides", "16.0.2.6/noupdate_changes.xml")
-    _fill_values_on_slide_slide(env)
-    _fill_nbr_article_on_slide_channel(env)
